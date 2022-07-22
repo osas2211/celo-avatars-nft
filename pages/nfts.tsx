023ad52529212644873645b7d31d273e23d8f3f2
Osaretin Frank <osaretin.frank10@gmail.com>
2022-07-22 17:04:26 +0100

    userCard added

--- pages/nfts.tsx
+++ pages/nfts.tsx
@@ -8,6 +8,7 @@ import { useCeloAvatarContract } from "../utils/hooks"
 import { getUserNfts } from "../utils/avatarNFT"
 import UserNftCard from "../components/AvatarNFT/UserNftCard"
 import Loader from "./loader"
+import { log } from "console"
 
 const Nfts = () => {
 	const [loading, setLoading] = useState(false)
@@ -21,11 +22,9 @@ const Nfts = () => {
 			// fetch all nfts from the smart contract
 			//@ts-ignore
 			const allNfts: [] = await getUserNfts(celoAvatarContract, { address })
-			console.log(allNfts)
-			// if (!allNfts) return
-			// setUserNfts(allNfts)
+			if (!allNfts) return
+			setUserNfts(allNfts)
 			setLoading(false)
-
 		} catch (error) {
 			console.log({ error })
 		}
@@ -33,8 +32,7 @@ const Nfts = () => {
 	useEffect(() => {
 		getUserAssets()
 	}, [getUserAssets])
-
-
+	console.log(userNfts)
 
 	return (
 		<div>
@@ -43,31 +41,29 @@ const Nfts = () => {
 					<Link href={"/"}>Home</Link>
 				</div>
 				<header className="nft-header">Your NFTs</header>
-				{!loading? (
+				{!loading ? (
 					<div className="container">
-					<div className="row justify-content-center align-items-center gap-2">
-						{userNfts.length !== 0 ? (
-							userNfts.map((nft, key) => {
-								return (
-									<div className="col-md-4 col-8" key={key}>
-										<UserNftCard nft={nft} />
-									</div>
-								)
-							})
-						) : (
-							<>
-								<p className="text-center display-5 text-muted">
-									No Nft Available
-								</p>
-							</>
-						)}
+						<div className="row justify-content-center align-items-center gap-2">
+							{userNfts.length !== 0 ? (
+								userNfts.map((nft, key) => {
+									return (
+										<div className="col-md-4 col-8" key={key}>
+											<UserNftCard nft={nft} />
+										</div>
+									)
+								})
+							) : (
+								<>
+									<p className="text-center display-5 text-muted">
+										No Nft Available
+									</p>
+								</>
+							)}
+						</div>
 					</div>
-				</div>
-
 				) : (
 					<Loader />
 				)}
-				
 			</div>
 		</div>
 	)
